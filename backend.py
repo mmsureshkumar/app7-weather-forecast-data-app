9a35a03e042a1b26b5f9ad6b864efbe08ec44d67
smurugas <mmsureshkumar@gmail.com>
2024-08-12 22:24:52 +0530

    Updated the images for the selection of SKY Added the images related to SKY

--- backend.py
+++ backend.py
@@ -3,20 +3,15 @@ import requests
 API_KEY = "8c0ade2abfa1956c57e1e2cb91981fcf"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    elif kind == "Sky":
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-
     return filtered_data
 
 
 if __name__ == "__main__":
-    print((get_data(place="Tokyo",forecast_days=3, kind="Sky")))
+    print((get_data(place="Tokyo",forecast_days=3)))
--- main.py
+++ main.py
@@ -12,7 +12,20 @@ kind = st.selectbox(label="Select Data to View", options=["Temperature", "Sky"])
 
 st.subheader(f"{kind} for the next {forecast_days} days in {place}")
 
-d, t = get_data(place, forecast_days, kind)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
-st.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place, forecast_days)
+        match kind:
+            case "Temperature":
+                temperature = [dict['main']['temp']/10 for dict in filtered_data]
+                dates = [dict['dt_txt'] for dict in filtered_data]
+                figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature"})
+                st.plotly_chart(figure)
+            case "Sky":
+                images = {'Clear' : 'images/clear.png', 'Clouds' : 'images/cloud.png', 'Rain' : 'images/rain.png', 'Snow' : 'images/snow.png'}
+                sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+                print(sky_conditions)
+                imagepaths = [images[x] for x in sky_conditions]
+                st.image(imagepaths, width=100)
+    except:
+            st.text("Place you entered is not exist. Kindly re-enter the place that exist")
